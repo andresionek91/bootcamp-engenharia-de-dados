3829b937ce08a494ca9259c848ca693143c6e499
Andre Sionek <andresionek91@gmail.com>
2020-08-17 21:35:45 +0100

    fix emr job

---
+++ 3.airflow_spark/4.spark_emr/convert_to_parket.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName('to_parquet') \
+    .getOrCreate()
+
+for coin in ['BTC', 'BCH', 'ETH', 'LTC']:
+    df = spark.read.format('json').load(f's3a://s3-belisco-production-data-lake-raw/cryptocurrency/{coin}')
+    df.write.format('parquet').save(path=f's3a://s3-belisco-production-data-lake-processed/cryptocurrency/{coin}', mode='overwrite')
+
+
