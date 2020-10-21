ac5f60fe8d5dee21230910b1fa79d0a5b9d50d04
Andre Sionek <andresionek91@gmail.com>
2020-10-21 23:09:33 +0100

    adiciona funcao para renderizar template ao deploy

--- turma_2/2_integracao_continua_e_testes/2.jinja/deploy.py
+++ turma_2/2_integracao_continua_e_testes/2.jinja/deploy.py
@@ -1,8 +1,10 @@
-from processa_template import renderiza_template
 import boto3
 import logging
 from botocore.exceptions import ClientError
+import jinja2
+import yaml
 import os
+
 
 logging.getLogger().setLevel(logging.INFO)
 cloudformation_client = boto3.client('cloudformation')
@@ -75,8 +77,23 @@ def create_or_update_stack():
         create_stack(stack_name, template_body)
 
 
-if __name__ == '__main__':
+def renderiza_template():
     logging.info(f'RENDERING JINJA')
-    renderiza_template()
+    with open(_get_abs_path('redshift.yml.j2', 'r')) as f:
+        redshift_yaml = f.read()
+
+    with open(_get_abs_path('config.yml', 'r')) as f:
+        config = yaml.safe_load(f)
+
+
+    redshift_template = jinja2.Template(redshift_yaml)
+    redshift_rendered = redshift_template.render({**config, **os.environ})
+
+    with open(_get_abs_path('redshift.yml', 'w')) as f:
+        f.write(redshift_rendered)
     logging.info(f'JINJA RENDERED')
+
+
+if __name__ == '__main__':
+    renderiza_template()
     create_or_update_stack()
