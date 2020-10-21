37d830fb5b65dad1f59228cc031eb56d8e683815
Andre Sionek <andresionek91@gmail.com>
2020-10-21 23:11:56 +0100

    corrige bug

--- turma_2/2_integracao_continua_e_testes/2.jinja/deploy.py
+++ turma_2/2_integracao_continua_e_testes/2.jinja/deploy.py
@@ -79,17 +79,17 @@ def create_or_update_stack():
 
 def renderiza_template():
     logging.info(f'RENDERING JINJA')
-    with open(_get_abs_path('redshift.yml.j2', 'r')) as f:
+    with open(_get_abs_path('redshift.yml.j2'), 'r') as f:
         redshift_yaml = f.read()
 
-    with open(_get_abs_path('config.yml', 'r')) as f:
+    with open(_get_abs_path('config.yml'), 'r') as f:
         config = yaml.safe_load(f)
 
 
     redshift_template = jinja2.Template(redshift_yaml)
     redshift_rendered = redshift_template.render({**config, **os.environ})
 
-    with open(_get_abs_path('redshift.yml', 'w')) as f:
+    with open(_get_abs_path('redshift.yml'), 'w') as f:
         f.write(redshift_rendered)
     logging.info(f'JINJA RENDERED')
 
