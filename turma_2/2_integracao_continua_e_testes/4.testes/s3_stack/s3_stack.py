62ab9b81359ee2ae60f8d9cac6db044dc88d19d0
Andre Sionek <andresionek91@gmail.com>
2020-10-22 21:40:14 +0100

    muda nome do bucket

--- turma_2/2_integracao_continua_e_testes/4.testes/s3_stack/s3_stack.py
+++ turma_2/2_integracao_continua_e_testes/4.testes/s3_stack/s3_stack.py
@@ -9,4 +9,4 @@ class S3Stack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        s3.Bucket(self, 'meu-bucket-belisco-cdk', bucket_name='meu-bucket-belisco-cdk')
+        s3.Bucket(self, 'meu-bucket-belisco-cdk-testes', bucket_name='meu-bucket-belisco-cdk-testes')
