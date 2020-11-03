96a353f4db439790ff51556b209e8805c86d8c02
Andre Sionek <andresionek91@gmail.com>
2020-11-03 20:07:21 +0000

    adiciona aula 5 - airflow

--- turma_1/3.airflow_spark/4.spark_emr/convert_to_parket.py
+++ turma_1/3.airflow_spark/4.spark_emr/convert_to_parket.py
@@ -7,5 +7,3 @@ spark = SparkSession.builder \
 for coin in ['BTC', 'BCH', 'ETH', 'LTC']:
     df = spark.read.format('json').load(f's3a://s3-belisco-production-data-lake-raw/cryptocurrency/{coin}')
     df.write.format('parquet').save(path=f's3a://s3-belisco-production-data-lake-processed/cryptocurrency/{coin}', mode='overwrite')
-
-
